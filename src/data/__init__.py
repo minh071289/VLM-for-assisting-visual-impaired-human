eefe36cb4586cc260ab74cfa28882c403eac6bb6
huyvanzzz <160515209+huyvanzzz@users.noreply.github.com>
2026-03-07 12:11:37 +0700

    Update __init__.py

--- src/data/__init__.py
+++ src/data/__init__.py
@@ -1,6 +1,6 @@
 from .wad_dataset import WADDataset, build_dataset
 from .data_collator import VLMDataCollator
-from .preprocessing import POLMData, GroundTruthData, construct_prompt, map_metadata_to_ground_truth
+from .preprocessing import GroundTruthData, construct_prompt, map_metadata_to_ground_truth
 
 __all__ = [
     'WADDataset',
